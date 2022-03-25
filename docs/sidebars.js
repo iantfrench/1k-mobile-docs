235e0c05469d0e64576f0926bdf23bbaf5c0e578
iantfrench <93606113+iantfrench@users.noreply.github.com>
2022-03-25 12:13:03 -0700

    updated for nodejs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -63,14 +63,6 @@ const sidebars = {
       items: [
         "mobile-sdk/mobile-sdk-overview",
         "mobile-sdk/mobile-sdk-api-reference",
-        /*{
-          type: "category",
-          label: "API Reference",
-          items: [
-            "mobile-sdk/api-reference/mobile-sdk-api-reference-ios",
-            "mobile-sdk/api-reference/mobile-sdk-api-reference-android",
-          ],
-        },*/
         {
           type: "category",
           label: "Integration Guide",
